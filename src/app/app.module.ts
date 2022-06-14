c628c9b890a69fa5ab6a7033577805868e0cbd4f
JosueZelay <josuezelaya@unah.hn>
2022-06-14 14:12:37 -0600

    Borrando
    prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {routing, appRoutingProviders} from './app.routing';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndexComponent } from './components/index/index.component';
-import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
-    IniciarSesionComponent
+
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    routing
+
   ],
   providers: [
-    appRoutingProviders
+  
   ],
   bootstrap: [AppComponent]
 })
