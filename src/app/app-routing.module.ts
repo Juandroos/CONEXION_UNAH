7791991df767f4693ce6af65ea084bdd70b25090
Juandros <juan1alejandro1@gmail.com>
2022-06-14 14:09:20 -0600

    REGISTRO + API
    En los siguiente se encontrara lo relacionado con la pagina de registro, conexion a la BDD de Mongol.
    
    Tambien las operaciones de PUT, GET, DELETE, POST.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//components
+import { ListaUsuariosComponent } from './components/lista-usuarios/lista-usuarios.component';
+import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
+
+const routes: Routes = [
+  { path: '', component: ListaUsuariosComponent},
+  { path: 'registrar-usuario',component: RegistrarUsuarioComponent},
+  { path: 'editar-usuario/:id', component:RegistrarUsuarioComponent},
+  { path: '**', redirectTo:'', pathMatch: 'full'}//DEBE SER SIEMPRE LA ULTIMA
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule} from '@angular/common/http';
+
+// Componentes
 import { AppComponent } from './app.component';
+import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
+import { ListaUsuariosComponent } from './components/lista-usuarios/lista-usuarios.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrarUsuarioComponent,
+    ListaUsuariosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule, 
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
