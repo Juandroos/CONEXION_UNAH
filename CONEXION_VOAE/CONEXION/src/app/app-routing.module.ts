20afbe534e0bc3bfa5777edd227f23d2e9618a9f
Juandros <juan1alejandro1@gmail.com>
2022-06-26 01:27:38 -0600

    ROUTES
    Debe modificar las rutas de correo. Se encuentra fuera de components.

--- CONEXION_VOAE/CONEXION/src/app/app-routing.module.ts
+++ CONEXION_VOAE/CONEXION/src/app/app-routing.module.ts
@@ -6,16 +6,17 @@ import { ListaUsuariosComponent } from './components/lista-usuarios/lista-usuari
 import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
 import { LoginComponent } from './components/login/login.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { CorreoComponent } from './correo/correo.component';
+import { CorreoComponent } from './correo/correo.component';//ESTA RUTE DEBE SEGUIR EL FORMATO DE LAS DEMAS!
+import { InicioComponent } from './components/inicio/inicio.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent},
-  { path: 'inicio', component: LandingPageComponent},
   { path: 'registrar-usuario',component: RegistrarUsuarioComponent},
   { path: 'editar-usuario/:id', component:RegistrarUsuarioComponent},
   { path: 'correo',component:CorreoComponent},
   { path: 'login', component:LoginComponent},
   { path: 'landing-page', component:LandingPageComponent},
+  { path: 'inicio', component:InicioComponent},
   { path: '**', redirectTo:'', pathMatch: 'full'}//DEBE SER SIEMPRE LA ULTIMA
 
 ];
